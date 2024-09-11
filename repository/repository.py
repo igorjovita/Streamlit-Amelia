f6c0e7cfe69210cfd7fab94f0c2103fd249dbc3c
IgorJovita <igor.jovita@hotmail.com>
2024-09-11 16:46:29 -0300

    Update repository.py

--- repository/repository.py
+++ repository/repository.py
@@ -305,7 +305,7 @@ class Repository:
             c.nome
         FROM agendamento_condominios as a
         JOIN condominio as c ON c.id = a.id_condominio
-        WHERE DATE_FORMAT(a.data_agendamento, '%Y-%m) = %s""", (mes_ano, ))
+        WHERE DATE_FORMAT(a.data_agendamento, '%Y-%m') = %s""", (mes_ano, ))
 
         resultado = cursor.fetchall()
         self.db.disconnect()
